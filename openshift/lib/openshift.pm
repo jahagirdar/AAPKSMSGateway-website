fa1414d0dc98bb868a6973760e4f05a2aa2f9bd5
vijayvithal <jahagirdar.vs@gmail.com>
2014-03-19 01:00:07 +0530

    Added Database code

--- openshift/lib/openshift.pm
+++ openshift/lib/openshift.pm
@@ -10,16 +10,16 @@ get '/donation/view/list' => sub {
     
 	my @row = database->quick_select('donor', {  });
         template 'display_donor_list', { widget => \@row };
-    };
+};
 get '/donation/view/:id' => sub {
 	my $row = database->quick_select('donor', { id => 42 });
 	template 'display_donor', { widget => $row };
-    };
-    post '/donation/add' =>sub{
-	    
-        database->quick_insert('donor', { name => params->{name} });
-    
-    }
+};
+post '/donation/add' =>sub{
+
+    database->quick_insert('donor', { name => params->{name} });
+
+};
 get '/donation' => sub {
     "Hello, World!";
 };
