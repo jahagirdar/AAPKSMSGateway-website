c066d222a5744e63b9820f6ab909040e4c98316f
vijayvithal <jahagirdar.vs@gmail.com>
2014-03-19 16:27:17 +0530

    debugging db error

--- openshift/lib/openshift.pm
+++ openshift/lib/openshift.pm
@@ -2,22 +2,22 @@ package openshift;
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
 our $VERSION = '0.1';
-
+my $dbh = database({ driver => 'mysql', database=>'fundraiser',host=>"mysql://$OPENSHIFT_MYSQL_DB_HOST",port=>$OPENSHIFT_MYSQL_DB_PORT,username=>'adminKcmiYjK',password=>'dP2tJsSDvSrz',connection_check_threshold=>10});
 get '/' => sub {
     template 'index';
 };
 get '/donation/view/list' => sub {
     
-	my @row = database->quick_select('donor', {  });
+	my @row = $dhb->quick_select('donor', {  });
         template 'display_donor_list', { widget => \@row };
 };
 get '/donation/view/:id' => sub {
-	my $row = database->quick_select('donor', { id => 42 });
+	my $row = $dbh->quick_select('donor', { id => 42 });
 	template 'display_donor', { widget => $row };
 };
 post '/donation/add' =>sub{
 
-    database->quick_insert('donor', { name => params->{name} });
+    $dbh->quick_insert('donor', { name => params->{name} });
 
 };
 get '/donation' => sub {
