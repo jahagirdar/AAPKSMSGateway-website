766d53a245392ceb36dd783c6ee4acaf39b9238c
vijayvithal <jahagirdar.vs@gmail.com>
2014-03-21 17:21:06 +0530

    db debug

--- openshift/lib/openshift.pm
+++ openshift/lib/openshift.pm
@@ -3,14 +3,25 @@ use Dancer ':syntax';
 use Dancer::Plugin::Database;
 our $VERSION = '0.1';
 #my $dbh = database({ driver => 'mysql', database=>'fundraiser',host=>$ENV{OPENSHIFT_MYSQL_DB_HOST},port=>$ENV{OPENSHIFT_MYSQL_DB_PORT},username=>'adminKcmiYjK',password=>'dP2tJsSDvSrz',connection_check_threshold=>10});
-my $dbh = database({ database=>"$ENV{'OPENSHIFT_DATA_DIR'}/fundraiser"});
+my $dbh = database();
 get '/' => sub {
-    template 'index';
+	template 'index';
+};
+get '/donation/create/volunteer' => sub{
+	  my $sth = database->prepare(
+            'CREATE TABLE volunteer ( phone_number TEXT PRIMARY KEY, name         TEXT, id           TEXT, amount       TEXT, role         TEXT, creator      TEXT );'
+        );
+        $sth->execute();
+	  my $sth = database->prepare(
+            'CREATE TABLE donation ( id TEXT PRIMARY KEY, donor_phone TEXT, name TEXT, volunteer_phone TEXT, amount TEXT, receipt TEXT, time TEXT );'
+        );
+        $sth->execute();
+    "Database Created";
 };
 get '/donation/view/list' => sub {
-    
+
 	my @row = $dbh->quick_select('donation', {  });
-        template 'display_donor_list', { widget => \@row };
+	template 'display_donor_list', { table => \@row };
 };
 get '/donation/view/:id' => sub {
 	my $row = $dbh->quick_select('donation', { id => 42 });
@@ -18,11 +29,11 @@ get '/donation/view/:id' => sub {
 };
 post '/donation/add' =>sub{
 
-    $dbh->quick_insert('donation', { Name => params->{name},
-	   Donor_Phone=>params->{dn_phone},
-  Volunteer_Phone=>params->{vol_phone},
-Donation_Time=>params->{don_time},
-Receipt=>params->{rec_number} });
+	$dbh->quick_insert('donation', { Name => params->{name},
+			Donor_Phone=>params->{dn_phone},
+			Volunteer_Phone=>params->{vol_phone},
+			Donation_Time=>params->{don_time},
+			Receipt=>params->{rec_number} });
 
 };
 get '/donation' => sub {
