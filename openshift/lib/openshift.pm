2719509a6998a08124f4348e4eeea81f576d4b59
vijayvithal <jahagirdar.vs@gmail.com>
2014-03-23 11:06:31 +0530

    db debug

--- openshift/lib/openshift.pm
+++ openshift/lib/openshift.pm
@@ -2,8 +2,8 @@ package openshift;
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
 our $VERSION = '0.1';
-#my $dbh = database({ driver => 'mysql', database=>'fundraiser',host=>$ENV{OPENSHIFT_MYSQL_DB_HOST},port=>$ENV{OPENSHIFT_MYSQL_DB_PORT},username=>'adminKcmiYjK',password=>'dP2tJsSDvSrz',connection_check_threshold=>10});
-my $dbh = database();
+my $dbh = database({ driver => 'mysql', database=>'fundraiser',host=>$ENV{OPENSHIFT_MYSQL_DB_HOST},port=>$ENV{OPENSHIFT_MYSQL_DB_PORT},username=>'adminKcmiYjK',password=>'dP2tJsSDvSrz',connection_check_threshold=>10});
+#my $dbh = database();
 get '/' => sub {
 	template 'index';
 };
